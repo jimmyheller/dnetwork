86d19132995321c1c4bf6066c54359f710241eaf
rahmani.z <1366@R>
2019-08-22 13:48:08 +0430

    deleteing not used imports

--- src/test/java/com/dnetwork/dnetwork/DNetUserRepositoryTest.java
+++ src/test/java/com/dnetwork/dnetwork/DNetUserRepositoryTest.java
@@ -3,13 +3,10 @@ package com.dnetwork.dnetwork;
 import com.dnetwork.domain.DNetUser;
 import com.dnetwork.domain.DNetUserRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
